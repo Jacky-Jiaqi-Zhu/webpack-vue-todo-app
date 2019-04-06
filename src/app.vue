8c4b7b123e4a92b239e2c3d46b11e05dc7a52231
Jacky <jacky@mindatlas.com>
2019-04-06 11:02:32 +1100

    setup webpack-dev-server

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="test">{{text}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'abcddd'
+        }
+    }
+}
+</script>
+
+
+<style>
+#test {
+    color: red;
+}
+</style>
