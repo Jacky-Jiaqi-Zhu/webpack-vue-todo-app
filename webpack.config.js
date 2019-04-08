abfb898cb76a24d262f8daf9a63d6e43eab527fa
Jacky <jiaqi.zhu.jacky@gmail.com>
2019-04-08 21:59:36 +1000

    extract vender and webpack related code

--- webpack.config.js
+++ webpack.config.js
@@ -89,6 +89,11 @@ if(isDev) {
         new webpack.NoEmitOnErrorsPlugin()
     )
 }else{
+    config.entry = {
+        app: path.join(__dirname, 'src/index.js'),
+        vendor: ['vue'],
+
+    }
     config.output.filename = '[name].[chunkhash:8].js'
     config.module.rules.push(
         {
@@ -109,7 +114,13 @@ if(isDev) {
         }
     )
     config.plugins.push(
-        new ExtractPlugin('styles.[contentHash:8].css')
+        new ExtractPlugin('styles.[contentHash:8].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'runtime'
+        })
     )
 }
 
